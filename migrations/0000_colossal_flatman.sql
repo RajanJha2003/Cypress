e28971f4514ede076f31ed5cb5423927dc761cab
Rajan <rajanj448@student.sfit.ac.in>
2024-05-23 18:54:56 +0530

    final

---
+++ migrations/0000_colossal_flatman.sql
@@ -0,0 +1,222 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+DO $$ BEGIN
+ CREATE TYPE "key_status" AS ENUM('default', 'valid', 'invalid', 'expired');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "key_type" AS ENUM('aead-ietf', 'aead-det', 'hmacsha512', 'hmacsha256', 'auth', 'shorthash', 'generichash', 'kdf', 'secretbox', 'secretstream', 'stream_xchacha20');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_type" AS ENUM('totp', 'webauthn');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_status" AS ENUM('unverified', 'verified');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "aal_level" AS ENUM('aal1', 'aal2', 'aal3');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "code_challenge_method" AS ENUM('s256', 'plain');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "one_time_token_type" AS ENUM('confirmation_token', 'reauthentication_token', 'recovery_token', 'email_change_token_new', 'email_change_token_current', 'phone_change_token');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "equality_op" AS ENUM('eq', 'neq', 'lt', 'lte', 'gt', 'gte', 'in');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "action" AS ENUM('INSERT', 'UPDATE', 'DELETE', 'TRUNCATE', 'ERROR');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "pricing_type" AS ENUM('one_time', 'recurring');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "pricing_plan_interval" AS ENUM('day', 'week', 'month', 'year');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "subscription_status" AS ENUM('trialing', 'active', 'canceled', 'incomplete', 'incomplete_expired', 'past_due', 'unpaid');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"full_name" text,
+	"avatar_url" text,
+	"billing_address" jsonb,
+	"updated_at" timestamp with time zone,
+	"payment_method" jsonb,
+	"email" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "customers" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"stripe_customer_id" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "products" (
+	"id" text PRIMARY KEY NOT NULL,
+	"active" boolean,
+	"name" text,
+	"description" text,
+	"image" text,
+	"metadata" jsonb
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "prices" (
+	"id" text PRIMARY KEY NOT NULL,
+	"product_id" text,
+	"active" boolean,
+	"description" text,
+	"unit_amount" bigint,
+	"currency" text,
+	"type" "pricing_type",
+	"interval" "pricing_plan_interval",
+	"interval_count" integer,
+	"trial_period_days" integer,
+	"metadata" jsonb
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "subscriptions" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" uuid NOT NULL,
+	"status" "subscription_status",
+	"metadata" jsonb,
+	"price_id" text,
+	"quantity" integer,
+	"cancel_at_period_end" boolean,
+	"created" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+	"current_period_start" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+	"current_period_end" timestamp with time zone DEFAULT timezone('utc'::text, now()) NOT NULL,
+	"ended_at" timestamp with time zone DEFAULT timezone('utc'::text, now()),
+	"cancel_at" timestamp with time zone DEFAULT timezone('utc'::text, now()),
+	"canceled_at" timestamp with time zone DEFAULT timezone('utc'::text, now()),
+	"trial_start" timestamp with time zone DEFAULT timezone('utc'::text, now()),
+	"trial_end" timestamp with time zone DEFAULT timezone('utc'::text, now())
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "collaborators" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"workspace_id" uuid NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"user_id" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "files" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"title" text NOT NULL,
+	"icon_id" text NOT NULL,
+	"data" text,
+	"in_trash" text,
+	"banner_url" text,
+	"workspace_id" uuid NOT NULL,
+	"folder_id" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "folders" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"title" text NOT NULL,
+	"icon_id" text NOT NULL,
+	"data" text,
+	"in_trash" text,
+	"banner_url" text,
+	"workspace_id" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "workspaces" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"workspace_owner" uuid NOT NULL,
+	"title" text NOT NULL,
+	"icon_id" text NOT NULL,
+	"data" text,
+	"in_trash" text,
+	"logo" text,
+	"banner_url" text
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "prices" ADD CONSTRAINT "prices_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_price_id_fkey" FOREIGN KEY ("price_id") REFERENCES "prices"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_price_id_prices_id_fk" FOREIGN KEY ("price_id") REFERENCES "prices"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "collaborators" ADD CONSTRAINT "collaborators_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "collaborators" ADD CONSTRAINT "collaborators_workspace_id_workspaces_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "workspaces"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "files" ADD CONSTRAINT "files_folder_id_folders_id_fk" FOREIGN KEY ("folder_id") REFERENCES "folders"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "files" ADD CONSTRAINT "files_workspace_id_workspaces_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "workspaces"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "folders" ADD CONSTRAINT "folders_workspace_id_workspaces_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "workspaces"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+*/
